ba503587090cedbae1d60222ab944f23a733e2c3
darkknightxi <kartikeyakhare11@gmail.com>
2023-06-08 16:48:17 +0530

    Update page.js

--- jd-generator/src/app/page.js
+++ jd-generator/src/app/page.js
@@ -1,6 +1,31 @@
+"use client"
+import { useState } from "react";
+
 export default function Home() {
+  const [apiKey, setApiKey] = useState("");
+
+  /* Add more logic here */
+
   return (
-    <div>hello world</div>
-  )
+    <div className="flex flex-col h-screen">
+      {/* Navbar */}
+      <nav className="bg-white shadow w-full">
+        <div className="px-4 h-14 flex justify-between items-center">
+          <div className="text-xl font-bold">Use AI to Generate Job Decriptions</div>
+          <div>
+            <input
+              type="password"
+              className="border rounded p-1"
+              placeholder="Enter API key.."
+              value={apiKey}
+              onChange={(e) => setApiKey(e.target.value)}
+            />
+          </div>
+        </div>
+      </nav>
+
+      {/* Add more UI here */}
+    </div>
+  );
 }
  
